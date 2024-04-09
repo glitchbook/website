c428a62515c67c56224987d726ded5c898e02a72
Aetinx <sargentcoding@gmail.com>
2024-04-09 20:13:20 +0000

    Temporary video version

--- src/images.js
+++ src/images.js
@@ -36,25 +36,33 @@ const loop = (element, imageset) => {
   t()
 }
 
-loop(document.getElementById("ag1"), [
+loop(document.getElementById("ag-selfies"), [
+  // Skirt indirect back
   "https://i.ibb.co/HzZ56J3/Xfs-JQvv-Y58o9-Lgm-H82-T7vd2-Bfd-TELFa-T-1-format-jpg-name-medium.jpg",
-  "https://i.ibb.co/b7VMMKw/GDRx-PMVXQAAx-Tq-format-jpg-name-medium.jpg",
-  "https://i.ibb.co/58YghQ6/F7xx-JYVWEAA1-MXB-format-jpg-name-medium.jpg",
+  // Emo standoffish
   "https://i.ibb.co/Zd0XR1K/IMG-20240128-184449351-format-jpg-name-medium.jpg",
+  // Emo smile direct
   "https://i.ibb.co/2kMFd4r/IMG-20240128-183939976-format-jpg-name-medium.jpg"
 ])
-loop(document.getElementById("ag2"), [
-  "https://i.ibb.co/nkHWFRM/GDZ8jdz-Xc-AAa-Dwv-format-jpg-name-medium.jpg",
-  "https://i.ibb.co/7bdBGJF/GDmj-Ksy-W4-AA009f-format-jpg-name-medium.jpg",
-  "https://i.ibb.co/0fKL6tj/ce8-Thumbnail20012024172-format-jpg-name-medium.jpg",
+
+loop(document.getElementById("ag-work"), [
+  // Emoji one
   "https://i.ibb.co/5WfMWg2/a54-Thumbnail20012024172-format-jpg-name-medium.jpg",
+  // Emoji many
   "https://i.ibb.co/G56spNb/7e3-Thumbnail20012024172-format-jpg-name-medium.jpg",
+  // Kitt
   "https://i.ibb.co/pZbQ0Zy/kitt-Thumbnail-format-jpg-name-medium.jpg",
+  // Latapals
   "https://i.ibb.co/Y8w4TNG/Twitter-post-4-format-jpg-name-medium.jpg"
 ])
-loop(document.getElementById("ag3"), [
+
+loop(document.getElementById("ag-captures"), [
+  // Truck
   "https://res.cloudinary.com/dzlxuyf7c/image/upload/q_auto,w_1080,c_limit,f_auto/UPLOAD_MEDIA/mp9251oqahoaqwrp50vf",
+  // Pride flag
   "https://res.cloudinary.com/dzlxuyf7c/image/upload/q_auto,w_1080,c_limit,f_auto/UPLOAD_MEDIA/cnl2hx0asm5kazo4sigt",
+  // Flowers out
   "https://res.cloudinary.com/dzlxuyf7c/image/upload/q_auto,w_1080,c_limit,f_auto/UPLOAD_MEDIA/y48nd0fhlnemlg2cwrl9",
+  // Flowers in
   "https://res.cloudinary.com/dzlxuyf7c/image/upload/q_auto,w_1080,c_limit,f_auto/UPLOAD_MEDIA/wugd9dksycle1cuavf5l"
 ])
